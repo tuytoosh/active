1773a5e619cf502fb29a875d4547d41e0f47b985
Hamid <>
2022-05-16 01:23:52 +0430

    it can handle array of routes now

--- src/Core.php
+++ src/Core.php
@@ -7,16 +7,31 @@ class Core
     public function active($route, $class = null)
     {
         $class = $class ?? config('active.class') ?? 'active';
+        $currentRouteName = app('router')->current()->getName();
 
+        if (is_array($route))
+            return $this->arrayHandler($route, $currentRouteName, $class);
+
+        return $this->matches($route, $currentRouteName) ? $class : "";
+    }
+    public function matches($route, $currentRouteName) {
         if ($route == '*') {
-            return $class;
+            return true;
         }
 
-        $currentRouteName = app('router')->current()->getName();
         if (Str::endsWith($route, "*")) {
             $start_with = Str::replaceLast("*", "", $route);
-            return Str::startsWith($currentRouteName, $start_with) ? $class : "";
+            return Str::startsWith($currentRouteName, $start_with);
         }
-        return $route == $currentRouteName ? $class : "";
+
+        return $route == $currentRouteName;
+
+    }
+    public function arrayHandler($routes, $currentRouteName, $class) {
+        foreach($routes as $route) {
+            if ($this->matches($route, $currentRouteName))
+                return $class;
+        }
+        return "";
     }
 }
---
+++ tests/ArrayTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Active\Tests;
+
+
+use Active\Core;
+use Illuminate\Support\Facades\Route;
+use Active\Facade;
+use Active\Tests\TestCase;
+
+
+class ArrayTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        Route::shouldReceive('current')->andReturnSelf();
+        Route::shouldReceive('getName')->andReturn('home');
+    }
+
+    /** @test */
+    public function it_can_handle_arrays()
+    {
+        Route::shouldReceive('current')->andReturnSelf();
+        Route::shouldReceive('getName')->andReturn('home');
+        $this->assertEquals('active', app('active')->active(['home', 'about'], 'active'));
+        $this->assertEquals("", app('active')->active([], 'active'));
+    }
+}
